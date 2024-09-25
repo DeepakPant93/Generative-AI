24b1ff28723be2216db6c74efe7e8f811110ab80
deepakpant <deepak.93p@gmail.com>
2024-09-25 21:05:39 +0530

    Added ollama openai llm and ollama openai chatbot

--- YogaChatBot/main.py
+++ YogaChatBot/main.py
@@ -3,6 +3,12 @@
 
 if __name__ == "__main__":
     from src.chatgpt.app import start_chat as start_chatgpt
+    from src.ollama.app import start_chat as start_ollama
 
     ## Happy Chatting...
-    start_chatgpt()
+
+    ## Start ChatGPT
+    start_chatgpt() ## Use this line if you want to use ChatGPT
+
+    ## Start Ollama
+    # start_ollama() ## Uncomment this line if you want to use Ollama
---
+++ YogaChatBot/src/ollama/app.py
@@ -0,0 +1,44 @@
+# Copyright (c) 2024 AwesomeHelpersInc. All rights reserved.
+# This file is part of the YogaGPT project.
+
+import os
+
+import streamlit as st
+from dotenv import load_dotenv
+from langchain_core.output_parsers import StrOutputParser
+from langchain_core.prompts import ChatPromptTemplate
+from langchain_community.llms import Ollama
+
+load_dotenv()
+
+## Langmith tracking
+os.environ["LANGCHAIN_TRACING_V2"] = "true"
+os.environ["LANGCHAIN_API_KEY"]=os.getenv("LANGCHAIN_API_KEY")
+
+ERROR_MSG = 'You exceeded your current quota, please check your plan and billing details. For more information contact to the system administrator.'
+
+
+def start_chat():
+    ## Prompt Template
+    prompt = ChatPromptTemplate.from_messages(
+        [
+            ("system",
+             "You are a yoga assistant. Please response to the user queries based on there Yoga related questions. You're only eligible to take questions related to Yoga only, like what is yoga, pranayam and how to cure with yoga."),
+            ("user", "Question:{question}")
+        ]
+    )
+    ## streamlit framework
+    st.title('Your Personal Yoga Assistant')
+    input_text = st.text_input("Search anything related to Yoga...")
+    # openAI LLm
+    llm = Ollama(model="llama2")
+    output_parser = StrOutputParser()
+    chain = prompt | llm | output_parser
+    if input_text:
+        output = ERROR_MSG
+        try:
+                output = chain.invoke({'question': input_text})
+        except Exception as e:
+            print('Error::', str(e))
+            pass
+        st.write(output)
