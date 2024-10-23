de0ab408548ef577dfe1f38a94258118683428a0
DeepakPant93 <DeepakPant93@users.noreply.github.com>
2024-10-23 14:14:09 +0000

    deploy: bd0b2039c2ce36d047852ccf3df2fff2a7554695

--- YogaGPT/main.py
+++
@@ -1,7 +0,0 @@
-# Copyright (c) 2024 AwesomeHelpersInc. All rights reserved.
-# This file is part of the YogaGPT project.
-
-if __name__ == "__main__":
-    from src import yoga_gpt
-
-    yoga_gpt.run_yoga_gpt()
